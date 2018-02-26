f16bb557a4613b22e3c3e21d26adf74943f9d0df
logicmyth <staley77@protonmail.com>
2018-02-26 16:06:36 -0500

    add ui.js with function to display data

--- auth/api.js
+++ auth/api.js
@@ -2,7 +2,7 @@ var getMovieList = (channelTargeted) => {
   return new Promise((resolve, reject) => {
   //console.log(channelTargeted);
     return $.ajax({
-      url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=bbb0e77b94b09193e6f32d5fac7a3b9c&language=en-US&region=greece',
+      url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=bbb0e77b94b09193e6f32d5fac7a3b9c&language=en-US&region=GRC',
       method: 'GET',
       success: (response) => {
         resolve(response);
