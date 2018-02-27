bbb24b219d2745a52d18b7d867650c3ffbe50f13
logicmyth <staley77@protonmail.com>
2018-02-27 12:29:30 -0500

    fix api call url. pull title

--- auth/api.js
+++ auth/api.js
@@ -5,7 +5,7 @@ var getMovieList = () => {
   return new Promise((resolve, reject) => {
   //console.log(channelTargeted);
     return $.ajax({
-      url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=bbb0e77b94b09193e6f32d5fac7a3b9c&language=en-US&region=GRC',
+      url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=bbb0e77b94b09193e6f32d5fac7a3b9c&language=en-US&region=GR',//
       method: 'GET',
       success: (response) => {
         console.log(response + "successResponse!");
@@ -16,7 +16,7 @@ var getMovieList = () => {
       },
     }).then(function(data){
       console.log(".done");
-      var jsonData = JSON.stringify(data);
+      var jsonData = JSON.stringify(data.results[0].title);
       console.log(jsonData+"hi");
       $('.results').html(jsonData);
     });
