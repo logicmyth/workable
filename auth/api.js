e307c80f50294180ef09ba8d84421a9029343b60
logicmyth <staley77@protonmail.com>
2018-02-27 21:16:26 -0500

    store api data in array in display it.

--- auth/api.js
+++ auth/api.js
@@ -1,24 +1,51 @@
 'use strict';
 
 var getMovieList = () => {
-  console.log("getMovieList");
+  //console.log("getMovieList");
   return new Promise((resolve, reject) => {
-  //console.log(channelTargeted);
     return $.ajax({
-      url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=bbb0e77b94b09193e6f32d5fac7a3b9c&language=en-US&region=GR',//
+      url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=bbb0e77b94b09193e6f32d5fac7a3b9c&language=en-US&region=GR&append_to_response=credits',//&original_title
       method: 'GET',
       success: (response) => {
-        console.log(response + "successResponse!");
+        //console.log(response + "successResponse!");
         resolve(response);
       },
       error: (error) => {
         reject(error);
       },
     }).then(function(data){
-      console.log(".done");
-      var jsonData = JSON.stringify(data.results[0].title);
-      console.log(jsonData+"hi");
-      $('.results').html(jsonData);
-    });
+      console.log(data);
+      var jsonArray = [];
+      for (var i=0; i < data.results.length; i++) {
+        //console.log('forloop');
+        jsonArray.push(data.results[i].title + data.results[i].original_title + data.results[i].overview);
+      };
+      //JSON.stringify(jsonArray);
+      //console.log(jsonArray +"hi");
+      $('.results').html(jsonArray);
+      return specificMovie;
+    }).then(function(specificMovie){
+      return $.ajax({
+        url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=bbb0e77b94b09193e6f32d5fac7a3b9c&language=en-US&region=GR&append_to_response=credits',//&original_title
+        method: 'GET',
+        success: (response) => {
+          console.log(response + "successResponse!");
+          resolve(response);
+        },
+        error: (error) => {
+          reject(error);
+        },
+      })
+  }).then(function(){
+  var directors = [];
+  var getDirectors = function (data){
+      data.credits.crew.forEach(function(entry){
+    if (entry.job === 'Director') {
+      directors.push(entry.name);
+    }
+      });
+      var directorData = getDirectors();
+    };
   });
+});
 };
